7f0ac7bd45098b823d5cae1a77ca1b30e2ced3e3
rhazeDev <agcanasjustine1@gmail.com>
2025-10-16 21:13:20 +0800

    Add global styles and update stylesheet links in login and register pages

--- login.php
+++ login.php
@@ -52,7 +52,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Mechakeys</title>
-    <link href="styles.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
 
